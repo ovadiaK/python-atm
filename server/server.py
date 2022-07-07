f7e78c496748736bbcc5ddd2ab24165e6dc4aa46
ovadiaK <ykelenzon@gmail.com>
2022-07-07 21:43:10 +0300

    health checks

--- server/server.py
+++ server/server.py
@@ -1,3 +1,20 @@
+import http
+
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+
+@app.route("/health")
+def health():
+    resp = make_response("pong", 200)
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 def start():
     return "OK"
 
--- server/test_server.py
+++ server/test_server.py
@@ -2,7 +2,7 @@ from pytest_bdd import scenario, given, when, then
 from pathlib import Path
 import pytest
 
-from server import start, endpoint
+from server import start, endpoint, app
 
 featureFile = 'server.feature'
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,25 +12,32 @@ print(FEATURE_FILE)
 
 
 def pytest_configure():
+    pytest.server = app
     pytest.response = ''
 
 
 @scenario(FEATURE_FILE.__str__(), 'server starts and passed health checks')
-def test_server_world():
+def test_server():
     print("End of server test")
     pass
 
 
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
 @given('server is running')
-def empty_string():
-    start()
+def running():
+    print("running")
 
 
 @when('querying the health endpoint')
-def set_string_to_hello_world():
-    pytest.response = endpoint()
+def set_string_to_hello_world(client):
+    pytest.response = client.get("/health")
 
 
 @then('server responds with status ok')
 def string_equals_hello_world():
-    assert pytest.response == "200"
+    assert pytest.response.data == b'pong'
